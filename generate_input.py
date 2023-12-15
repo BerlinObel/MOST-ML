c782bf0a76a5bf9b1d25e0925b330cefd377655e
BerlinObel <oscarobel@gmail.com>
2023-12-15 13:29:36 +0100

    Create generate_input.py

---
+++ generate_input.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from tqdm import tqdm
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors as rdMD
+from rdkit.Chem import Descriptors
+import logging
+
+path ='MOST-ML/test.txt'
+
+mol_list = []
+with open(path, 'r') as f:
+    for line in f:
+        mol_list.append(line.strip())
+
+all_new_molecules_smiles = {Chem.MolToSmiles(Chem.MolFromSmiles(mol), canonical=True) for mol in mol_list}
+
+logging.info(f"Generated a total of {len(all_new_molecules_smiles)} new molecules.")
+
+
+input_df = pd.DataFrame(columns=['comp_name', 'smiles', 'charge', 'multiplicity'])
+
+for smiles in tqdm(all_new_molecules_smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    comp_hash = get_hashed_label(smiles)
+    comp_name = f"azo_{comp_hash}"
+    charge = 0
+    multiplicity = 1 
+
+    input_df = pd.concat([input_df, pd.DataFrame([{'comp_name': comp_name, 'smiles': smiles, 'charge': charge, 'multiplicity': multiplicity}])], ignore_index=True)
+
+input_df['molecular_weight'] = input_df['smiles'].apply(lambda x: Descriptors.MolWt(Chem.MolFromSmiles(x)))
+
+
+# save to csv
+input_df.to_csv('molecules.csv', index=False)
